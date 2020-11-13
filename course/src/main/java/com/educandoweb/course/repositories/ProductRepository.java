c310c50accaf29195c9e8ab80bf2af760d11debe
lfernando488 <lfernando488@gmail.com>
2020-11-13 03:56:09 -0300

    Course API

---
+++ course/src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.educandoweb.course.entities.Product;
+
+@Repository // Annotation é opcional pois ja herda de JPA Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
